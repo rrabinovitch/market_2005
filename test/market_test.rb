28522c2d1a4b87a195098ed13c5e08f71970bb98
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-10 09:36:03 -0600

    Set up market class and test

---
+++ test/market_test.rb
@@ -0,0 +1,54 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @vendor3.stock(@item1, 65)
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
+
+  def test_it_can_add_vendors
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
+  end
+
+  def test_it_can_list_vendor_names
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
+  end
+
+  def test_it_can_list_vendors_that_sell_specific_item
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
+    assert_equal [@vendor2], @market.vendors_that_sell(@item4)
+  end
+end
